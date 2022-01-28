fe3fa0aee304afac28ed6cd07df33c4858113a44
jaime Avendaño <jaime.aven.villa@gmail.com>
2022-01-27 20:41:12 -0500

    Price symbol

--- src/component/newItem/newItem.js
+++ src/component/newItem/newItem.js
@@ -1,8 +1,17 @@
 import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
-import React from 'react';
+import React, { useState } from 'react';
 import { Icon } from 'react-native-elements';
 
 const NewItem = () => {
+  const [element, setElement] = useState({
+    name: '',
+    price: '',
+  });
+
+  const onChangePrice = (value) => {
+    setElement({ ...element, price: value})
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.label}>Product</Text>
@@ -13,13 +22,16 @@ const NewItem = () => {
           placeholder='Add a product'
           />
         </View>
-        <View>
+        <View style={{ flexDirection: 'row' }}>
+          <Text style={{ paddingTop: 7, fontSize: 16, fontWeight: 'bold' }}>$</Text>
           <TextInput 
-            style={styles.input}
+            style={[styles.input, {marginLeft: 0} ]}
             placeholder='Add a Price' 
-            keyboardType='number-pad'/>
+            keyboardType='number-pad'
+            value={element.price}
+            onChangeText={(value) => { onChangePrice(value) }}/>
         </View>
-        <TouchableOpacity style={{ paddingTop: 10 }} >
+        <TouchableOpacity style={{ paddingLeft: 5, paddingTop: 10 }} >
           <Icon 
             name='plus-circle'
             type='feather' /> 
@@ -36,7 +48,8 @@ const styles = StyleSheet.create({
         flex: 1,
     },
     input: {
-      fontSize: 16,
+      paddingTop: 5,
+      fontSize: 19,
       marginHorizontal: 10,
     },
     label: {
