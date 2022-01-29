9573d702d532bcc72e37c3b5efa60f61cc2c2e96
jaime Avendaño <jaime.aven.villa@gmail.com>
2022-01-29 15:58:31 -0500

    data test

--- App.js
+++ App.js
@@ -1,16 +1,28 @@
 import React, { useState } from 'react';
 import { ThemeProvider } from 'react-native-elements';
 import Header from './src/component/Header/header';
+import Items from './src/component/Items/Items';
 import NewItem from './src/component/newItem/newItem'
 
 
 const App = () => {
-  const [list, setList] = useState([]);
+  const [list, setList] = useState([
+    {
+      name: 'Product 1',
+      price: '$18272',
+    },
+    {
+      name: 'Product 2',
+      price: '$77133'
+    }
+
+  ]);
 
   return (
     <ThemeProvider>
       <Header />
       <NewItem />
+      <Items productList={list} />
     </ThemeProvider>
   );
 };
