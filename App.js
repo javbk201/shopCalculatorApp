bcf4ba97b6ba5049588d41a234df5bd5e6a711ea
jaime Avendaño <jaime.aven.villa@gmail.com>
2022-02-09 17:36:28 -0500

    Status bar color

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { ThemeProvider } from 'react-native-elements';
+import { SafeAreaView, StatusBar } from 'react-native';
 import Header from './src/component/Header/header';
 import Items from './src/component/Items/Items';
 import NewItem from './src/component/newItem/newItem'
@@ -17,10 +18,14 @@ const App = () => {
 
   return (
     <ThemeProvider>
-      <Header />
-      <NewItem setState={setState} state={state} />
-      <Items productList={state.list} setState={setState} state={state}  />
-      <TotalComponent productList={state.list} />
+      <SafeAreaView style={{ flex: 0, backgroundColor: '#3730a3' }} />
+      <SafeAreaView style={{ flex: 1, }}>
+        <StatusBar backgroundColor={'#3730a3'} barStyle={'light-content'} />
+        <Header />
+        <NewItem setState={setState} state={state} />
+        <Items productList={state.list} setState={setState} state={state}  />
+        <TotalComponent productList={state.list} />
+      </SafeAreaView>
     </ThemeProvider>
   );
 };
